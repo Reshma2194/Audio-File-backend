6c5fb1baa57138772134b26ef02c4cad8e84f3a0
Reshma M Nair <102210571+Reshma2194@users.noreply.github.com>
2022-11-24 17:08:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next)=>{
     next();
 });
 
-mongoose.connect("mongodb://localhost:27017/audiobackend");
+mongoose.connect("mongodb+srv://reshma:reshma1234@cluster0.esm57oj.mongodb.net/audioproject");
 let db = mongoose.connection;
 db.on("error", error=>console.log(error));
 db.on("open", ()=>console.log("Connection Established"));
